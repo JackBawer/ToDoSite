0413e213bc0f11bce3b99b345c65ba7c33d4773d
Jack Bawer <jackb2507@gmail.com>
2025-06-04 07:50:04 +0100

    Add base config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/ToDoSite/", // 👈 This is the key part for GitHub Pages
   plugins: [react()],
-})
+});
